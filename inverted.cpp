7fe89afe95758bb03389702d8fd2841c71d8665d
Misha Ryabko <michael0ryabko@gmail.com>
2023-12-14 13:48:16 -0800

    adding cuda and c++

---
+++ inverted.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <fstream>
+
+typedef struct {
+  unsigned int term;
+  unsigned int doc;
+  unsigned int count;
+} triple;
+
+
+int main(int argc, char **argv) {
+  if(argc != 2) {
+    std::cout << "Usage: " << argv[0] << " <input_file> " << std::endl;
+    return -1;
+  }
+
+  std::string input_file(argv[1]);
+  std::ifstream triples_file(input_file, std::ios::binary);
+  std::vector<triple> triples;
+
+  if(!triples_file.is_open()) {
+    std::cout << argv[1] << " is an invalid file" << std::endl;
+    return -1;
+  }
+  
+  std::string line;
+  while(std::getline(triples_file, line)) {
+    std::istringstream string_stream(line);
+    std::string current_value;
+    std::vector<unsigned int> current_triple;
+    
+    while(std::getline(string_stream, current_value, ',')) {
+      current_triple.push_back(std::atoi(current_value.c_str()));
+    }
+
+    if(current_triple.size() != 3) {
+      std::cout << "invalid length triple, ignoring" << std::endl;
+
+      continue;
+    }
+
+    triples.emplace_back(triple({current_triple[0], current_triple[1], current_triple[2]}));
+  }
+
+  std::sort(triples.begin(), triples.end(), [](triple a, triple b)
+  {
+    return a.term == b.term ? a.doc < b.doc : a.term < b.term;
+  });
+  
+  for (auto &i: triples) {
+    std::cout << i.term << "," << i.doc << "," << i.count << std::endl;
+  }
+
+  return 0;
+}
